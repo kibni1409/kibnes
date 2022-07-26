a42e530285004122bd756c8e13a1fa6f2409e25e
kibni <kibni1409@gmail.com>
2022-07-26 16:00:14 +0300

    Updates

---
+++ precache-manifest.4d63567750f237b2fcbb60ac89d2ab62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "74671fb53e519e71a8da",
+    "url": "/kibnes/static/css/main.60ac52bc.chunk.css"
+  },
+  {
+    "revision": "74671fb53e519e71a8da",
+    "url": "/kibnes/static/js/main.f556f972.chunk.js"
+  },
+  {
+    "revision": "44822db8215c5d67929d",
+    "url": "/kibnes/static/js/runtime~main.ff2d6a7b.js"
+  },
+  {
+    "revision": "a9402a0fd31142be2028",
+    "url": "/kibnes/static/js/2.b897769f.chunk.js"
+  },
+  {
+    "revision": "00584ff8e6ea18161f09f90400c4027d",
+    "url": "/kibnes/static/media/1_1WCjO1iYMo7J7Upp8KMfLA@2x.00584ff8.jpeg"
+  },
+  {
+    "revision": "57615b8c0092a66c1d4058b1692955cc",
+    "url": "/kibnes/static/media/XOsX.57615b8c.gif"
+  },
+  {
+    "revision": "e5469feeab1097f78887f838f6bd0935",
+    "url": "/kibnes/index.html"
+  }
+];
